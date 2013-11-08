e38aa648e0840513025b5b51e9804455e27261a5
Roman Dobrovensky <heller@heller.ru>
2013-11-08 14:52:14 +0400

    Multipliers and so on

--- tex2wp.py
+++ tex2wp.py
@@ -1,5 +1,9 @@
 import re
 
+# Todo:
+# end-of-proof sign
+# corollary environment
+
 def convert(filename):
     with open(filename, encoding="utf-8") as src:
         with open("out.txt", "w") as dst:
@@ -11,8 +15,26 @@ def convert(filename):
                 line = re.sub(r"<<", "&laquo;", line)
                 line = re.sub(r">>", "&raquo;", line)
                 line = re.sub(r"\\S", "&sect;", line)
-                line = re.sub(r"\\begin\{exercise\}",
-                              "<strong>Упражнение</strong>", line)
                 line = re.sub(r"\\end\{exercise\}", "", line)
+                line = re.sub(r"\\end\{thm\}", "", line)
+                line = re.sub(r"\\end\{proof\}", "", line)
+                line = re.sub(r"\\end\{example\}", "", line)
+                line = re.sub(r"\\end\{definition\}", "", line)
+                line = re.sub(r"\\term\{(.+?)\}", "<i>\g<1></i>", line)
+                
+                oldlen = len(line)
+                line = re.sub(r"\\begin\{exercise\}",
+                              "\n<strong>Упражнение.</strong>", line)
+                line = re.sub(r"\\begin\{thm\}",
+                              "\n<strong>Теорема.</strong>", line)
+                line = re.sub(r"\\begin\{proof\}",
+                              "<strong>Доказательство.</strong>", line)
+                line = re.sub(r"\\begin\{example\}",
+                              "\n<strong>Пример.</strong>", line)
+                line = re.sub(r"\\begin\{definition\}",
+                              "\n<strong>Определение.</strong>", line)
+                if oldlen != len(line):
+                    line = line.rstrip()
+                    line += " "
                 dst.write(line)
 
